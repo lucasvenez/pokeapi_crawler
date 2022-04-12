39f542f727f9e803f59ab7076f58d7eed91aa362
lysialeao <lysialeao@gmail.com>
2022-04-12 11:29:13 -0300

    Iniciando a classe PokeAPI

--- lysia_leao/main.py
+++ lysia_leao/main.py
@@ -1,31 +1,17 @@
-import requests 
 
-def get_pokemon(*list):
+class PokeAPI():
+	def __init__(self, value):
+		self.value = value
+	
+	def imprimir(self): 
+		print(self.value)
+		
 
-  result = []
+poke1 = PokeAPI(10)
+poke2 = PokeAPI(5)
+poke3 = PokeAPI(1)
 
-  for id in list:
+poke1.imprimir()
+poke2.imprimir()
+poke3.imprimir()
 
-    request = requests.get("https://pokeapi.co/api/v2/pokemon/"+str(id))
-    response= request.json()
-
-    name = response['name']
-    height = response['height']
-    weight = response['weight']
-    ability = response['abilities']
-    
-    result.append(('Nome do pokémon: ', name))
-    result.append(('Altura', height))
-    result.append(('Peso', weight))
-  
-    result.append('Habilides: ')
-    
-    for s in ability:
-      abilitys = s['ability']['name']
-      result.append((abilitys))
-      
-    result.append('------------------------------------------------------------------')
-
-  return result
-
-get_pokemon(1,2,3,4,5,6)
