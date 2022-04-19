a15b5b4429406dba0e6dc62ebd55f66bfb3a0a1e
lysialeao <lysialeao@gmail.com>
2022-04-19 09:02:26 -0300

    Change the solution for sum

--- lysia_leao/soma.py
+++ lysia_leao/soma.py
@@ -1,9 +1,9 @@
 numero = int(input("Digite um número: "))
 
 def soma(n:int):
-  result = 0
-  for i in range(1, n+1):
-    result += i
+  result = n  * (n + 1)/2
+  # for i in range(1, n+1):
+    # result += i
   return result
    
 soma(numero)
