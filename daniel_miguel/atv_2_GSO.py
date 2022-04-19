e93c5792d71ef19974eec12cd31ff5400a8c724e
danielmhsouza <dmhsouza@gmail.com>
2022-04-19 10:46:00 -0300

    subindo alteração na pasta

--- daniel_miguel/atv_2_GSO.py
+++ daniel_miguel/atv_2_GSO.py
@@ -5,15 +5,10 @@ r = requests.get('https://pokeapi.co/api/v2/pokemon/2')
 
 
 class pokeClass():
-    def __init__(self, vl):
-        self.value = vl
-    def a(self):
-        print(self.value)
+    def __init__(self):
+        print("nada")
 
-i = pokeClass(10)
-i2 = pokeClass(5)
-i3 = pokeClass(1)
+poke1 = pokeClass()
 
-i.a()
-i2.a()
-i3.a()
+
+
